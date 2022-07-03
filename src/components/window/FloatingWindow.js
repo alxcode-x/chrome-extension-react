2d0b29481993694d7166cd9fcbe74b7e89ab3082
Alexis Lopez <alexis.sbtg@gmail.com>
2022-07-03 15:18:10 -0500

    Menu design completed in fixed position

--- src/components/window/FloatingWindow.js
+++ src/components/window/FloatingWindow.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Draggable from "react-draggable";
-import './FloatingWindow.css';
+import "./FloatingWindow.css";
+import Menu from "./menu/Menu";
 
 function FloatingWindow() {
 
     return (
-        <Draggable defaultPosition={{x:(window.innerWidth/2) - ((window.innerWidth/2)/4), y:(-window.innerHeight/2)- 120}}>
+        <Draggable defaultPosition={{x:(window.innerWidth/1.2) - ((window.innerWidth/2)/4), y:(-window.innerHeight/1.3)- 120}}>
             <div className="windowWrapper">
-                <div>Draggable window!</div>
+                <Menu/>
             </div>
         </Draggable>
     );
